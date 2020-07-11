cb3e24001563416e5ba103949356633b07e31551
Harman <harman052@gmail.com>
2020-07-11 14:14:40 +0200

    Updates

---
+++ precache-manifest.b6ec8c783b0b08aabadee10159490f99.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "667aa7f3ce675b0d0b3cbfce7938f03f",
+    "url": "/react-tutorial-solutions/index.html"
+  },
+  {
+    "revision": "2ddb2d720e2ad8302aa9",
+    "url": "/react-tutorial-solutions/static/css/main.c2456740.chunk.css"
+  },
+  {
+    "revision": "13c0425173494107c0fb",
+    "url": "/react-tutorial-solutions/static/js/2.a5ca6e5b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-tutorial-solutions/static/js/2.a5ca6e5b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2ddb2d720e2ad8302aa9",
+    "url": "/react-tutorial-solutions/static/js/main.ac9bc8a9.chunk.js"
+  },
+  {
+    "revision": "7a13090e4d07042daf3b",
+    "url": "/react-tutorial-solutions/static/js/runtime-main.d04aa953.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -11,13 +11,19 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-tutorial-solutions/precache-manifest.1cf51b23af0841e074ba401c408fe0f8.js"
+  "/react-tutorial-solutions/precache-manifest.b6ec8c783b0b08aabadee10159490f99.js"
 );
 
-workbox.clientsClaim();
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -25,10 +31,9 @@ workbox.clientsClaim();
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/react-tutorial-solutions/index.html", {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-tutorial-solutions/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
